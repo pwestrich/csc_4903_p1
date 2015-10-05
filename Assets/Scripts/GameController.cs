f795290a6dd94d1a91827a187a89f57bb7e3cdd6
Philip Westrich <pwestrich@me.com>
2015-10-05 12:05:42 -0500

    Added game controller and player controller.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public static GameController controller;
+
+	public PlayerController player;
+
+	public int lives;
+	public int health;
+	public int ammo;
+	
+	// Use this for initialization
+	void Awake(){
+
+		if (controller == null) {
+
+			controller = this;
+			DontDestroyOnLoad(gameObject);
+			OnLevelWasLoaded(0);
+
+		} else {
+
+			Destroy(gameObject);
+
+		}
+	
+	}
+
+	void OnLevelWasLoaded(int level){
+
+		health = 100;
+
+	}
+	
+	// Update is called once per frame
+	void Update(){
+	
+	}
+
+	public void die(){
+
+		lives--;
+
+		if (lives == 0) {
+
+			//print "YouLose" or something
+
+		} else {
+
+			//reload the level
+			Application.LoadLevel(Application.loadedLevel);
+
+		}
+
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float fireDelay;
+	public float fireDistance;
+
+	private float fireCooldown = 0.0f;
+
+	// Use this for initialization
+	void Start(){
+	
+	}
+	
+	void FixedUpdate(){
+
+		float fire = Input.GetAxis("Fire1");
+
+		if (fire > 0.0f && fireCooldown <= 0.0f) {
+
+			this.fire();
+			fireCooldown = fireDelay;
+
+		} else if (fireCooldown > 0.0f) {
+
+			fireCooldown -= Time.deltaTime;
+
+		}
+	
+	}
+
+	void OnTriggerEnter(Collider other){
+
+		if (other.CompareTag("Death")) {
+
+			//kill the player
+			GameController.controller.die();
+
+		}
+
+	}
+
+	private void fire(){
+
+		//animate gun
+
+		//do raycast
+		int middleX = Camera.main.pixelWidth / 2;
+		int middleY = Camera.main.pixelHeight / 2;
+		Ray ray = Camera.main.ScreenPointToRay(new Vector3(middleX, middleY, 0));
+		RaycastHit hit;
+
+		if (Physics.Raycast(ray, out hit, fireDistance)) {
+
+			GameObject obj = hit.collider.gameObject;
+
+			Debug.Log("Hit something: " + obj);
+
+		}
+
+	}
+}
+
