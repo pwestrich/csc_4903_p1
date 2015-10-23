1b4ac0a02eedbd78a924219f44617f8945c2daa6
Philip Westrich <pwestrich@me.com>
2015-10-23 12:58:21 -0500

    Added exit.

---
+++ Assets/Scripts/ExitController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitController : MonoBehaviour, ObjectController {
+
+	public int targetLevel;
+
+	public void performAction(GameObject player){
+
+		Application.LoadLevel(targetLevel);
+
+	}
+
+}
